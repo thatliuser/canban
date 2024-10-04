fdc43325c0a0859e68da553dbf4e0586005aeba5
Eric L <eliu15@uci.edu>
2024-10-03 22:56:02 -0700

    Parse Notion db properties kinda

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ mod canvas;
 mod notion;
 
 #[derive(serde::Deserialize)]
+struct DatabaseConfig {
+    id: String,
+}
+
+#[derive(serde::Deserialize)]
+struct NotionConfig {
+    token: String,
+    database: DatabaseConfig,
+}
+
+#[derive(serde::Deserialize)]
 struct Config {
     canvas_token: String,
-    notion_token: String,
+    notion: NotionConfig,
 }
 
 fn main() {
@@ -12,8 +23,9 @@ fn main() {
     let config: Config = serde_json::from_str(config.as_str()).expect("Couldn't parse config file");
 
     let canvas = canvas::CanvasClient::new(config.canvas_token);
-    let notion = notion::NotionClient::new(config.notion_token);
+    let notion = notion::NotionClient::new(config.notion.token);
 
+    /*
     let courses: Vec<canvas::Course> = canvas
         .courses()
         .into_iter()
@@ -24,6 +36,22 @@ fn main() {
         println!("{:?} ---", course);
         for assignment in canvas.assignments(&course) {
             println!("{:?}", assignment);
+        }
+    }
+    */
+
+    let db = notion.database(config.notion.database.id);
+    println!("{:?}", db);
+    for (key, prop) in db.properties {
+        match key.as_str() {
+            "status" => {}
+            "subject" => {}
+            _ => {}
+        }
+        match prop.inner {
+            notion::PropertyType::Select { options } => {}
+            notion::PropertyType::Status { options } => {}
+            _ => {}
         }
     }
 }
--- src/notion.rs
+++ src/notion.rs
@@ -1,4 +1,53 @@
 use reqwest::{blocking::Client, header::HeaderMap};
+use std::collections::HashMap;
+
+#[derive(serde::Deserialize, Debug)]
+pub struct PropOption {
+    pub id: String,
+    pub name: String,
+    pub color: String,
+    pub description: Option<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum PropertyType {
+    // Most of these are not properly implemented because I'm lazy
+    Checkbox {},
+    CreatedBy {},
+    CreatedTime {},
+    Date {/* Empty */},
+    Email {},
+    Files {},
+    Formula {},
+    LastEditedBy {},
+    LastEditedTime {},
+    MultiSelect {},
+    Number {},
+    People {},
+    PhoneNumber {},
+    Relation {},
+    RichText {},
+    Rollup {},
+    Select { options: Vec<PropOption> },
+    Status { options: Vec<PropOption> },
+    Title {/* Empty */},
+    Url {},
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Property {
+    pub id: String,
+    pub name: String,
+    #[serde(flatten)]
+    pub inner: PropertyType,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Database {
+    pub id: String,
+    pub properties: HashMap<String, Property>,
+}
 
 pub struct NotionClient {
     client: Client,
@@ -11,8 +60,22 @@ impl NotionClient {
             "Authorization",
             format!("Bearer {}", token).parse().unwrap(),
         );
+        headers.insert("Notion-Version", "2022-06-28".parse().unwrap());
         NotionClient {
             client: Client::builder().default_headers(headers).build().unwrap(),
         }
+    }
+
+    pub fn database(self: &Self, id: String) -> Database {
+        let url = format!("https://api.notion.com/v1/databases/{}", id);
+        let resp = self
+            .client
+            .get(url)
+            .send()
+            .expect("Couldn't fetch database from API")
+            .text()
+            .unwrap();
+        println!("{}", resp);
+        serde_json::from_str(resp.as_str()).expect("Couldn't parse database API response")
     }
 }
